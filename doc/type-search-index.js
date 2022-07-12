4f87b65e887881ab2ca564c64675b2020ff4b1c0
Gustavo Santos <g_henrique19@hotmail.com>
2022-07-13 00:33:20 +0100

    First Comment

---
+++ doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"Estudo"},{"p":"<Unnamed>","l":"Estudo2"},{"p":"<Unnamed>","l":"Estudo3"},{"p":"<Unnamed>","l":"Estudo3Test"},{"p":"<Unnamed>","l":"Executor"}];updateSearchResults();
---
+++ src/streams/CriandoStreams.java
@@ -0,0 +1,37 @@
+package streams;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+
+public class CriandoStreams {
+
+	public static void main(String[] args) {
+		
+		Consumer<String> print = System.out::print;
+		Consumer<Integer> println = System.out::println;
+		
+		Stream<String> langs = Stream.of("Java ", "Lua ", "JS\n");
+		String[] array = {"React ", "Angular ", "Vue.js ", "Node.js\n"};
+		langs.forEach(print);
+		
+		String[] maisLangs = { "Python ", "Lisp ", "Perl ", "Go\n" };
+		
+		Stream.of(maisLangs).forEach(print);
+		Arrays.stream(maisLangs).forEach(print);
+		Arrays.stream(maisLangs, 1, 4).forEach(print);
+		
+		List<String> outrasLangs = Arrays.asList("C ", "PHP ", "Kotlin\n");
+		List<String> outrasLangs1 = Arrays.asList(array);
+		
+		outrasLangs.stream().forEach(print);
+		outrasLangs.parallelStream().forEach(print);
+		
+		outrasLangs1.stream().forEach(print);
+		outrasLangs1.parallelStream().forEach(print);
+		
+		// Stream.generate(() -> "a").forEach(print);
+		//Stream.iterate(0, n -> n + 1).forEach(println);
+	}
+}
